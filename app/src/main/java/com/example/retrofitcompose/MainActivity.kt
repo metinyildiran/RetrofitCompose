c5b193b11c64e1c2ca5a1b519942c5583ebf72f9
Metin Yıldıran <metin.1464@gmail.com>
2022-08-30 11:25:20 +0300

    Added CryptoList

--- app/src/main/java/com/example/retrofitcompose/MainActivity.kt
+++ app/src/main/java/com/example/retrofitcompose/MainActivity.kt
@@ -3,16 +3,17 @@ package com.example.retrofitcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.retrofitcompose.model.CryptoModel
 import com.example.retrofitcompose.service.CryptoAPI
@@ -24,9 +25,6 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : ComponentActivity() {
-
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -72,6 +70,29 @@ fun MainScreen(){
 }
 
 @Composable
+fun CryptoList(cryptoList: List<CryptoModel>){
+    LazyColumn {
+        items(cryptoList){ crypto ->
+            CryptoRow(crypto = crypto)
+        }
+    }
+}
+
+@Composable
+fun CryptoRow(crypto: CryptoModel){
+    Column(modifier = Modifier.fillMaxWidth()
+        .background(color = MaterialTheme.colors.background)) {
+        Text(text = crypto.currency,
+            style = MaterialTheme.typography.h4,
+            modifier = Modifier.padding(2.dp),
+            fontWeight = FontWeight.Bold)
+        Text(text = crypto.price,
+            style = MaterialTheme.typography.h5,
+            modifier = Modifier.padding(2.dp))
+    }
+}
+
+@Composable
 fun AppBar(){
     TopAppBar {
         Row(modifier = Modifier.fillMaxWidth(),
@@ -86,6 +107,6 @@ fun AppBar(){
 @Composable
 fun DefaultPreview() {
     RetrofitComposeTheme {
-        MainScreen()
+        CryptoRow(crypto = CryptoModel("BTC", "9999"))
     }
 }
